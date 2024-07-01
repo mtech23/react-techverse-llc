181c9bff10434d2a690749469f8ff1066e8ba939
mtech23 <frontendmarcell@gmail.com>
2024-06-30 22:07:03 -0700

    first commit

---
+++ src/asserts/images/index.js
@@ -0,0 +1,103 @@
+
+import headerBurgerIcon from "./Header-Burger-Menu.png";
+import mainLogo from "./home-logo.png";
+import headerSearchIcon from "./search-icon.png";
+import platinumFeetText from "./platinum-feet-text.png";
+import buttonBackground from "./home-btn-bk.png";
+import legsImage01 from "./hero-img-01.png";
+import imgTopCorner from "./hero-img-top-curve.png";
+import imgBottomCorner from "./hero-img-bottom-curve.png";
+import firstBackground from "./home-section-bk.png";
+import hundredPercentage from "./hundred-percent.png";
+import homeImg01 from "./hero-img-02.png";
+import modelText from "./model-text.png";
+import modelImg01 from "./model-img-01.png";
+import modelImg02 from "./model-img-02.png";
+import modelImg03 from "./model-img-03.png";
+import modelImg04 from "./model-img-04.png";
+import modelImg05 from "./model-img-05.png";
+import modelImg06 from "./model-img-06.png";
+import modelImg07 from "./model-img-07.png";
+import modelCardTopCorner from "./login-page-top-gold-bar.png";
+import modelCardBottomCorner from "./login-page-bottom-gold-bar.png";
+import framePic from "./after-img-frame.png";
+import hoverableImg from "./hover-img.png";
+import mainFrameImg from "./frame-with-hover.png";
+import videoPlayIcon from "./video-play-icon.png";
+import howItWorksText from "./how-it-works-text.png";
+import howItWorksImg from "./how-it-works-img.png";
+import forClientText from "./for-clients-text.png";
+import forClientImg from "./for-clients-left-img.png";
+import forModelsText from "./for-models-text.png";
+import formodelImg01 from "./for_model_left_part.png";
+import formodelImg02 from "./for-models-img-02.png";
+import faqText from "./faq-text.png";
+import footerSocialIcon01 from "./social-icon-01.png";
+import footerSocialIcon02 from "./social-icon-02.png";
+import footerSocialIcon03 from "./social-icon-03.png";
+import footerSocialIcon04 from "./social-icon-04.png";
+import footerSocialIcon05 from "./social-icon-05.png";
+import footerSocialIcon06 from "./social-icon-06.png";
+import hotModelImg01 from "./model-swiper-img.png";
+import userProfilePic from "./profile-pic.png";
+import userProfilePicBackground from "./more-user-profile-pic.png";
+import moreUserProfilePic from "./more-user-profile-pic.png";
+import profileFeetImg01 from "./profile-feet-img-01.png";
+import profileFeetImg02 from "./profile-feet-img-02.png";
+import profileFeetImg03 from "./profile-feet-img-03.png";
+import profileFeetImg04 from "./profile-feet-img-04.png";
+import profileFeetImg05 from "./profile-feet-img-05.png";
+import profileFeetImg06 from "./profile-feet-img-06.png";
+
+
+export{
+    headerBurgerIcon,
+    mainLogo,
+    headerSearchIcon,
+    platinumFeetText,
+    buttonBackground,
+    legsImage01,
+    imgTopCorner,
+    imgBottomCorner,
+    firstBackground,
+    hundredPercentage,
+    homeImg01,
+    modelText,
+    modelImg01,
+    modelCardTopCorner,
+    modelCardBottomCorner,
+    modelImg02,
+    modelImg03,
+    modelImg04,
+    framePic,
+    hoverableImg,
+    mainFrameImg,
+    videoPlayIcon,
+    modelImg05,
+    modelImg06,
+    modelImg07,
+    howItWorksText,
+    howItWorksImg,
+    forClientText,
+    forClientImg,
+    forModelsText,
+    formodelImg01,
+    formodelImg02,
+    faqText,
+    footerSocialIcon01,
+    footerSocialIcon02,
+    footerSocialIcon03,
+    footerSocialIcon04,
+    footerSocialIcon05,
+    footerSocialIcon06,
+    hotModelImg01,
+    userProfilePic,
+    userProfilePicBackground,
+    moreUserProfilePic,
+    profileFeetImg01,
+    profileFeetImg02,
+    profileFeetImg03,
+    profileFeetImg04,
+    profileFeetImg05,
+    profileFeetImg06
+};
